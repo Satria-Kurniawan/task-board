ec165da2f473a52c86fa2f9c1feecd324e12ee5a
Satria-Kurniawan <satriakurniawan2001.com>
2024-04-23 21:09:20 +0800

    fix: build error

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,14 +1,20 @@
 import { arrayMove as dndKitArrayMove } from "@dnd-kit/sortable";
+import { Task } from "../components/Task/type";
 
-export const removeAtIndex = (array, index) => {
+export const removeAtIndex = (array: Task[], index: number) => {
   return [...array.slice(0, index), ...array.slice(index + 1)];
 };
 
-export const insertAtIndex = (array, index, item) => {
-  delete item.sortable;
-  return [...array.slice(0, index), item, ...array.slice(index)];
+export const insertAtIndex = (array: Task[], index: number, item: Task) => {
+  const newItem = item as Task & { sortable?: undefined };
+  delete newItem.sortable;
+  return [...array.slice(0, index), newItem, ...array.slice(index)];
 };
 
-export const arrayMove = (array, oldIndex, newIndex) => {
+export const arrayMove = (
+  array: Task[],
+  oldIndex: number,
+  newIndex: number
+) => {
   return dndKitArrayMove(array, oldIndex, newIndex);
 };
